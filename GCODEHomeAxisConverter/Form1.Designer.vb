10da6ccde7828b65b6fb99a57bc087cbb05047a2
Marc Roigé <marc.roige@outlook.com>
2021-01-31 11:48:32 +0100

    Add project files.

---
+++ GCODEHomeAxisConverter/Form1.Designer.vb
@@ -0,0 +1,336 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(26, 480)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(212, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Select GCODE file:"
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("MS Reference Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(45, 8)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(960, 84)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Convert your file from SolidWorks cam procesor to GCODE standard 3D Axis maxine. " &
+    "Allow the conversion from first line GCODE to each line GCODE. "
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(291, 482)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(992, 25)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "/..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = Global.GCODEHomeAxisConverter.My.Resources.Resources._2021_01_18_17h36_52
+        Me.PictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox2.Location = New System.Drawing.Point(672, 82)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(502, 375)
+        Me.PictureBox2.TabIndex = 3
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = Global.GCODEHomeAxisConverter.My.Resources.Resources._2021_01_18_17h37_30
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(49, 82)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(495, 375)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Yellow
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(374, 324)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(282, 25)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "EXAMPLE CONVERSION ->"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(291, 535)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(347, 20)
+        Me.TextBox1.TabIndex = 6
+        Me.TextBox1.Text = "G,M"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(26, 530)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(259, 25)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Input values to convert:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(664, 541)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(470, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Separate by coma. For example ""G"" is used by Gcodes as (G1 to GXXXX) , ""M"" code a" &
+    "s the same"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(26, 579)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(248, 25)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Comment identificator:"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(291, 584)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(347, 20)
+        Me.TextBox2.TabIndex = 11
+        Me.TextBox2.Text = "' "" / \ ( , % $"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(664, 588)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(409, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Separate by space.  The process skip the line if something start with these chara" &
+    "cters"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(1066, 743)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(217, 44)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Convert!!"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BackColor = System.Drawing.Color.Transparent
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(26, 677)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(183, 25)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "Skip paramaters"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(291, 630)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(347, 20)
+        Me.TextBox3.TabIndex = 11
+        Me.TextBox3.Text = "X,Y,Z,I"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(664, 680)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(307, 13)
+        Me.Label9.TabIndex = 12
+        Me.Label9.Text = "Separate by space.  The process skip these characters from file"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(291, 717)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(155, 17)
+        Me.CheckBox1.TabIndex = 15
+        Me.CheckBox1.Text = "Spece between commands"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(515, 717)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(114, 17)
+        Me.CheckBox2.TabIndex = 16
+        Me.CheckBox2.Text = "Delete line number"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(664, 637)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(634, 13)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "Separate by coma. Use these parameters to add the last ""Input values to convert"" " &
+    "found before it to avoid lines withous inputs values"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.BackColor = System.Drawing.Color.Transparent
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(26, 626)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(139, 25)
+        Me.Label11.TabIndex = 18
+        Me.Label11.Text = "Parameters "
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(291, 677)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(347, 20)
+        Me.TextBox4.TabIndex = 17
+        Me.TextBox4.Text = "O"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(850, 718)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 20
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(664, 721)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(171, 13)
+        Me.Label12.TabIndex = 21
+        Me.Label12.Text = "Decimal point value of the GCODE"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1328, 814)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "GCODE Home Axis Converter Version V1.1"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label12 As Label
+End Class
---
+++ GCODEHomeAxisConverter/Form1.vb
@@ -0,0 +1,309 @@
+﻿Imports System.IO
+Imports System.Runtime.CompilerServices
+Public Class Form1
+    Public ValorsGcodes() As String
+    Public ValorsForbiden() As String
+    Public ValorsSpecials() As String
+    Public CharactersToJump() As String
+    Public path As String = ""
+    Public dot As String = "."
+    Public GCODE As String = ""
+    Public SPACE As String = ""
+
+    Sub CrearArrays()
+        ReDim ValorsGcodes(0)
+        ReDim ValorsForbiden(0)
+        ReDim ValorsSpecials(0)
+        ReDim CharactersToJump(0)
+
+        Dim dimensio As Integer = 0
+        Dim valorProvisional As String = ""
+        For Each valor In TextBox2.Text
+            If valor = " " Then
+                dimensio += 1
+                ReDim Preserve ValorsForbiden(dimensio - 1)
+                ValorsForbiden(dimensio - 1) = valorProvisional
+                valorProvisional = ""
+            Else
+                valorProvisional = valorProvisional & valor
+            End If
+        Next
+        If TextBox1.Text.Length > 1 And TextBox1.Text <> " " Then
+            dimensio += 1
+            ReDim Preserve ValorsForbiden(dimensio - 1)
+            ValorsForbiden(dimensio - 1) = valorProvisional
+        End If
+        dimensio = 0
+        valorProvisional = ""
+        For Each valor In TextBox1.Text
+            If valor = "," Then
+                dimensio += 1
+                ReDim Preserve ValorsGcodes(dimensio - 1)
+                ValorsGcodes(dimensio - 1) = valorProvisional
+                valorProvisional = ""
+            Else
+                valorProvisional = valorProvisional & valor
+            End If
+        Next
+
+        If TextBox2.Text.Length > 1 And TextBox2.Text <> " " Then
+            dimensio += 1
+            ReDim Preserve ValorsGcodes(dimensio - 1)
+            ValorsGcodes(dimensio - 1) = valorProvisional
+        End If
+        dimensio = 0
+        valorProvisional = ""
+        For Each valor In TextBox3.Text
+            If valor = "," Then
+                dimensio += 1
+                ReDim Preserve ValorsSpecials(dimensio - 1)
+                ValorsSpecials(dimensio - 1) = valorProvisional
+                valorProvisional = ""
+            Else
+                valorProvisional = valorProvisional & valor
+            End If
+        Next
+        If TextBox3.Text.Length > 1 And TextBox3.Text <> " " Then
+            dimensio += 1
+            ReDim Preserve ValorsSpecials(dimensio - 1)
+            ValorsSpecials(dimensio - 1) = valorProvisional
+        End If
+        dimensio = 0
+        valorProvisional = ""
+        For Each valor In TextBox4.Text
+            If valor = "," Then
+                dimensio += 1
+                ReDim Preserve CharactersToJump(dimensio - 1)
+                CharactersToJump(dimensio - 1) = valorProvisional
+                valorProvisional = ""
+            Else
+                valorProvisional = valorProvisional & valor
+            End If
+        Next
+        If TextBox4.Text.Length > 0 And TextBox4.Text <> " " Then
+            dimensio += 1
+            ReDim Preserve CharactersToJump(dimensio - 1)
+            CharactersToJump(dimensio - 1) = valorProvisional
+        End If
+
+    End Sub
+    Sub convertFileToGcode()
+        path = Button1.Text
+        If Not File.Exists(path) Then
+            MsgBox("File doesn't exist. Please check the file and try it again.", MsgBoxStyle.OkOnly, "Error to load file")
+            Exit Sub
+        End If
+        SetSpace()
+        CrearArrays()
+        If ComboBox1.Text.IndexOf(".") > -1 Then dot = "." Else dot = ","
+        Dim name As String = SetNameFileFromFolder(path)
+
+        Dim reader As StreamReader = My.Computer.FileSystem.OpenTextFileReader(path)
+        Dim writer As StreamWriter = My.Computer.FileSystem.OpenTextFileWriter(name, 1)
+        Dim lastOption As String = ""
+        Dim a As String
+        Dim LastGcode As String = ""
+        Dim rsl As String = ""
+        Do
+            a = reader.ReadLine
+            rsl = LineConverter(a)
+            If rsl <> "" Then writer.WriteLine(rsl)
+        Loop Until a Is Nothing
+        writer.Close()
+        reader.Close()
+        MsgBox("Completed, " & vbCrLf & name, MsgBoxStyle.OkOnly, "The file is exported properly")
+        SaveDefaultValues()
+    End Sub
+    Sub SaveDefaultValues()
+        My.Settings.OriginPath = Button1.Text
+        My.Settings.GCODES = TextBox1.Text
+        My.Settings.CommentId = TextBox2.Text
+        My.Settings.Special = TextBox3.Text
+        My.Settings.Forbidden = TextBox4.Text
+        My.Settings.DotValue = ComboBox1.Text
+        My.Settings.Spaces = CheckBox1.Checked
+        My.Settings.NLines = CheckBox2.Checked
+
+    End Sub
+    Function SetNameFileFromFolder(path As String)
+
+        Dim name As String = IO.Path.GetFileNameWithoutExtension(path)
+        Dim ext As String = IO.Path.GetExtension(path)
+        path = IO.Path.GetDirectoryName(path)
+        For i = 1 To 10000000
+            If File.Exists(path & "\" & name & "_" & i & ext) = False Then
+                path = path & "\" & name & "_" & i & ext
+                Exit For
+            End If
+        Next
+
+        Return path
+    End Function
+    Sub SetSpace()
+        If CheckBox1.Checked = True Then
+            SPACE = " "
+        Else
+            SPACE = ""
+        End If
+
+    End Sub
+    Public Function LineConverter(StrToProcess As String)
+        If IsForbiden(StrToProcess) = 1 Then Return ""
+        StrToProcess = DeleteNValues(StrToProcess)
+        StrToProcess = SkipValues(StrToProcess)
+        GetGCODE(StrToProcess)
+        StrToProcess = AddGcode(StrToProcess)
+        StrToProcess = SetLineSpaces(StrToProcess)
+        Return StrToProcess
+    End Function
+    Function SetLineSpaces(StrToProcess As String)
+
+        Dim lletra As String = ""
+        Dim results As String = ""
+        For Each e In StrToProcess
+            If System.Text.RegularExpressions.Regex.IsMatch(e, "^[A-Za-z]+$") = True Then
+                If lletra <> "" Then
+                    lletra = SPACE & e
+                Else
+                    lletra = e
+                End If
+            End If
+            If e = " " Then
+                lletra = SPACE
+            Else
+                lletra = e
+            End If
+            results = results & lletra
+        Next
+        Return results
+    End Function
+    Function AddGcode(StrToProcess As String)
+        If IsNothing(StrToProcess) Then Return ""
+        Dim linia As String = ""
+        Dim NValue As Integer = 0
+        For Each e In StrToProcess
+            For Each element In ValorsSpecials
+                If e = element And NValue = 0 Then
+                    linia = linia & GCODE & SPACE
+                    NValue = 1
+                Else
+                    For Each valor In ValorsGcodes
+                        If e = valor Then NValue = 1
+                    Next
+                End If
+            Next
+            linia = linia & e
+        Next
+        If linia <> "" Or linia <> " " Then StrToProcess = linia
+        Return StrToProcess
+    End Function
+    Sub GetGCODE(StrToProcess As String)
+        If IsNothing(StrToProcess) Then StrToProcess = ""
+        Dim linia As String = ""
+        Dim NValue As Integer = 0
+        For Each element In ValorsGcodes
+            For Each e In StrToProcess
+                If NValue = 0 And e = element Then
+                    NValue = 1
+                    linia = e
+                ElseIf NValue = 1 Then
+                    If IsNumeric(e) Then
+                        If e <> " " Then linia = linia & e
+                    Else
+                        NValue = 0
+                    End If
+                End If
+            Next
+        Next
+        If linia <> "" Or linia <> " " And linia.Length > 0 Then GCODE = linia
+    End Sub
+    Function IsForbiden(StrToProcess As String)
+        If IsNothing(StrToProcess) Then StrToProcess = ""
+        For Each e In StrToProcess
+            If existValue(e, ValorsForbiden) Then Return 1
+        Next
+        Return 0
+    End Function
+    Function DeleteNValues(StrToProcess As String)
+        If IsNothing(StrToProcess) Then StrToProcess = ""
+        Dim linia As String = ""
+        Dim Nstart As Integer = 0
+        Dim NValue As Integer = 0
+        If CheckBox2.Checked = True Then
+            For Each e In StrToProcess
+                If Nstart = 0 And e = "N" Then
+                    NValue = 1
+                ElseIf NValue = 1 Then
+                    If IsNumeric(e) = False Then
+                        NValue = -1
+                        If e <> " " Then linia = linia & e
+                    End If
+                Else
+                    linia = linia & e
+                End If
+                Nstart = 1
+
+            Next
+            StrToProcess = linia
+        End If
+        Return StrToProcess
+    End Function
+    Function SkipValues(StrToProcess As String)
+        If IsNothing(StrToProcess) Then Return ""
+        If CharactersToJump.Count < 1 Then Return StrToProcess
+        Dim linia As String = ""
+        Dim Nstart As Integer = 0
+        Dim NValue As Integer = 0
+        For Each e In StrToProcess
+            For Each lletra In CharactersToJump
+                If IsNothing(lletra) Then Exit For
+                If NValue = 0 And e = lletra Then
+                    NValue = 1
+                ElseIf NValue = 1 Then
+                    If IsNumeric(e) = False Then
+                        NValue = -1
+                        If e <> " " Then linia = linia & e
+                    End If
+                Else
+                    linia = linia & e
+                End If
+            Next
+        Next
+        StrToProcess = linia
+        Return StrToProcess
+    End Function
+
+    Function existValue(checkString As String, arr_() As String)
+        For Each value In arr_
+            If value = checkString Then
+                Return 1
+            End If
+        Next
+        Return 0
+    End Function
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        convertFileToGcode()
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim msgooption As OpenFileDialog = New OpenFileDialog
+        If msgooption.ShowDialog() = DialogResult.OK Then
+            path = msgooption.FileName
+            Button1.Text = path
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox1.Items.Add("Dot (.)")
+        ComboBox1.Items.Add("Comma (,)")
+        ComboBox1.SelectedIndex = 0
+        Button1.Text = My.Settings.OriginPath
+        TextBox1.Text = My.Settings.GCODES
+        TextBox2.Text = My.Settings.CommentId
+        TextBox3.Text = My.Settings.Special
+        TextBox4.Text = My.Settings.Forbidden
+        ComboBox1.Text = My.Settings.DotValue
+        If My.Settings.NLines = True Then CheckBox2.Checked = True
+        If My.Settings.Spaces = True Then CheckBox1.Checked = True
+
+    End Sub
+End Class
